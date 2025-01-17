191146009a9e3cb333bbeee72b5c2e477e6ed4c8
Jibago <wani3327@gmail.com>
2025-01-17 18:42:20 +0900

    message pin

--- chzzk/src/client.rs
+++ chzzk/src/client.rs
@@ -1,3 +1,5 @@
+use reqwest::RequestBuilder;
+
 use crate::{
     channel::{Channel, ChannelId, ChannelLiveStatus, ChatAccessToken, ChatChannelId},
     error::{chain_error, Error},
@@ -16,6 +18,9 @@ pub struct ChzzkClient {
     nid: Option<Nid>,
     client: reqwest::Client,
 }
+
+#[derive(serde::Deserialize)]
+struct DropResponse {}
 
 impl ChzzkClient {
     pub fn new() -> Self {
@@ -40,19 +45,170 @@ impl ChzzkClient {
         self.nid = Some(nid);
     }
 
-    /// Send request to given URL.
+    fn chzzk(path: &str) -> ChzzkRequestBuilder {
+        ChzzkRequestBuilder::new(format!("https://api.chzzk.naver.com/{path}"))
+    }
+
+    fn game(path: &str) -> ChzzkRequestBuilder {
+        ChzzkRequestBuilder::new(format!("https://comm-api.game.naver.com/nng_main/{path}"))
+    }
+
+    /// Get channel live status of channel with given id.
+    /// ex) https://api.chzzk.naver.com/polling/v2/channels/1dac6492f81d89e261f692bb6b79ff57/live-status
     ///
     /// # Errors
     ///
-    /// This function will return an error if request fails, or response is not valid json.
-    async fn do_request<'a, T>(&self, full_path: String) -> Result<T, Error>
+    /// This function will return an error if request fails.
+    pub async fn get_channel_live_status(
+        &self,
+        channel_id: &ChannelId,
+    ) -> Result<ChannelLiveStatus, Error> {
+        let response_object =
+            Self::chzzk(format!("polling/v2/channels/{}/live-status", **channel_id).as_str())
+                .get()
+                .send::<Response<ChannelLiveStatus>>(&self)
+                .await?;
+
+        Ok(response_object.content)
+    }
+
+    /// Get user status of currently logged-in user (bot).
+    /// ex) https://comm-api.game.naver.com/nng_main/v1/user/getUserStatus
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if request fails.
+    pub async fn get_user_status(&self) -> Result<User, Error> {
+        let response_object = Self::game("v1/user/getUserStatus".into())
+            .get()
+            .send::<Response<User>>(&self)
+            .await?;
+        Ok(response_object.content)
+    }
+
+    /// Get AccessToken.
+    /// ex) https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=N1SMvY&chatType=STREAMING
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if request fails.
+    pub async fn get_access_token(
+        &self,
+        chat_id: &ChatChannelId,
+    ) -> Result<ChatAccessToken, Error> {
+        let response_object = Self::game(
+            format!(
+                "v1/chats/access-token?channelId={}&chatType=STREAMING",
+                **chat_id
+            )
+            .as_str(),
+        )
+        .get()
+        .send::<Response<ChatAccessToken>>(&self)
+        .await?;
+
+        if response_object.code == 42601 {
+            return Err("Broadcast is age-restricted, nidAuth, nidSession is required".into());
+        }
+
+        Ok(response_object.content)
+    }
+
+    /// Get channel information.
+    /// ex) https://api.chzzk.naver.com/service/v1/channels/9ae7d38b629b78f48e49fb3106218ff5
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if request fails.
+    pub async fn get_channel_info(&self, channel_id: &ChannelId) -> Result<Channel, Error> {
+        let response = Self::chzzk(format!("service/v1/channels/{}", **channel_id).as_str())
+            .get()
+            .send::<Response<Channel>>(&self)
+            .await?;
+
+        Ok(response.content)
+    }
+
+    pub async fn set_chat_notice(
+        &self,
+        channel_id: &ChannelId,
+        chat_id: Option<&ChatChannelId>,
+        sender: &ChannelId,
+        time: u64
+    ) -> Result<(), Error> {
+        let chat_id = match chat_id {
+            Some(x) => x,
+            None => {
+                &self
+                    .get_channel_live_status(channel_id)
+                    .await?
+                    .chat_channel_id
+            }
+        };
+
+        let payload = serde_json::json!({
+            "channelId": chat_id,
+            "chatType": "STREAMING",
+            // "extras": "{\"chatType\":\"STREAMING\",\"osType\":\"PC\",\"streamingChannelId\":\"1dac6492f81d89e261f692bb6b79ff57\",\"emojis\":{}}",
+            "message": "0",
+            "messageTime": time,
+            "messageUserIdHash": sender,
+            "streamingChannelId": channel_id,
+        });
+
+        Self::game("v1/chats/notices")
+            .post(Some(payload.to_string()))
+            .send::<DropResponse>(&self)
+            .await?;
+        Ok(())
+    }
+}
+
+impl Default for ChzzkClient {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+struct ChzzkRequestBuilder {
+    url: String,
+    is_method_post: bool,
+    body: Option<String>,
+}
+
+impl ChzzkRequestBuilder {
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            is_method_post: false,
+            body: None,
+        }
+    }
+
+    pub fn get(self) -> Self {
+        self
+    }
+
+    pub fn post(self, body: Option<String>) -> Self {
+        Self {
+            url: self.url,
+            is_method_post: true,
+            body,
+        }
+    }
+
+    async fn send<'a, T>(self, client: &ChzzkClient) -> Result<T, Error>
     where
         T: serde::de::DeserializeOwned,
     {
-        println!("request to: {full_path}.");
-        let mut request = self.client.get(full_path);
+        println!("request to: {}.", self.url);
+        let mut request = if self.is_method_post {
+            client.client.post(self.url).header("Content-Type", "application/json").body(self.body.unwrap_or(String::new()))
+        } else {
+            client.client.get(self.url)
+        };
 
-        if let Some(nid) = &self.nid {
+        if let Some(nid) = &client.nid {
             request = request.header("Cookie", format!("NID_AUT={};NID_SES={}", nid.aut, nid.ses));
         }
 
@@ -74,100 +230,6 @@ impl ChzzkClient {
             )))?;
 
         Ok(json)
-    }
-
-    async fn request_chzzk<'a, T>(&self, path: String) -> Result<T, Error>
-    where
-        T: serde::de::DeserializeOwned,
-    {
-        self.do_request(format!("https://api.chzzk.naver.com/{path}"))
-            .await
-    }
-
-    async fn request_game<'a, T>(&self, path: String) -> Result<T, Error>
-    where
-        T: serde::de::DeserializeOwned,
-    {
-        self.do_request(format!("https://comm-api.game.naver.com/nng_main/{path}"))
-            .await
-    }
-
-    /// Get channel live status of channel with given id.
-    /// ex) https://api.chzzk.naver.com/polling/v2/channels/1dac6492f81d89e261f692bb6b79ff57/live-status
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if request fails.
-    pub async fn get_channel_live_status(
-        &self,
-        channel_id: &ChannelId,
-    ) -> Result<ChannelLiveStatus, Error> {
-        let response_object = self
-            .request_chzzk::<Response<ChannelLiveStatus>>(format!(
-                "polling/v2/channels/{}/live-status",
-                **channel_id
-            ))
-            .await?;
-
-        Ok(response_object.content)
-    }
-
-    /// Get user status of currently logged-in user (bot).
-    /// ex) https://comm-api.game.naver.com/nng_main/v1/user/getUserStatus
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if request fails.
-    pub async fn get_user_status(&self) -> Result<User, Error> {
-        let response_object = self
-            .request_game::<Response<User>>("v1/user/getUserStatus".into())
-            .await?;
-
-        Ok(response_object.content)
-    }
-
-    /// Get AccessToken.
-    /// ex) https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=N1SMvY&chatType=STREAMING
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if request fails.
-    pub async fn get_access_token(
-        &self,
-        chat_id: &ChatChannelId,
-    ) -> Result<ChatAccessToken, Error> {
-        let response_object = self
-            .request_game::<Response<ChatAccessToken>>(format!(
-                "v1/chats/access-token?channelId={}&chatType=STREAMING",
-                **chat_id
-            ))
-            .await?;
-
-        if response_object.code == 42601 {
-            return Err("Broadcast is age-restricted, nidAuth, nidSession is required".into());
-        }
-
-        Ok(response_object.content)
-    }
-
-    /// Get channel information.
-    /// ex) https://api.chzzk.naver.com/service/v1/channels/9ae7d38b629b78f48e49fb3106218ff5
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if request fails.
-    pub async fn get_channel_info(&self, channel_id: &ChannelId) -> Result<Channel, Error> {
-        let response = self
-            .request_chzzk::<Response<Channel>>(format!("service/v1/channels/{}", **channel_id))
-            .await?;
-
-        Ok(response.content)
-    }
-}
-
-impl Default for ChzzkClient {
-    fn default() -> Self {
-        Self::new()
     }
 }
 
--- examples/auth.rs
+++ examples/auth.rs
@@ -1,17 +1,12 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-
-/// Simple wrapper function that read two lines from `auth.txt` and return.
-/// You need to fill `auth.txt` with your own AUT and SES values from browser cookies
+/// You need to fill `.env` with your own AUT and SES values from browser cookies
 /// in order to run some examples.
 pub fn get_aut_ses_from_env() -> (String, String) {
-    let file = File::open("auth.txt").unwrap();
-    let reader = io::BufReader::new(file);
-
-    // Collect the first two lines.
-    let mut lines = reader.lines();
-    let aut = lines.next().unwrap().unwrap();
-    let ses = lines.next().unwrap().unwrap();
-
-    (aut, ses)
+    if dotenvy::dotenv().is_err() {
+        // success when run in examples directory
+        dotenvy::from_path(r#"examples/.env"#).unwrap(); // success when run in chzzk-rs root
+    }
+    (
+        std::env::var("CHZZK_AUT").unwrap(),
+        std::env::var("CHZZK_SES").unwrap(),
+    )
 }
