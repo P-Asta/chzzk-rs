cbcb62d485746d84eb44d6dd9e40bb88275d6f79
Asta(아스타) <86705803+P-Asta@users.noreply.github.com>
2025-03-14 09:23:37 +0900

    refactor(data/search_live): remove unnecessary code

--- chzzk/src/data/search_live.rs
+++ chzzk/src/data/search_live.rs
@@ -7,41 +7,25 @@ pub struct LiveSearchBundle {
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash, Default)]
+#[serde(rename_all="camelCase")]
 pub struct SearchedLive {
-    #[serde(rename = "liveTitle")]
     pub live_title: String,
-    #[serde(rename = "liveImageUrl")]
     pub live_image_url: String,
-    #[serde(rename = "defaultThumbnailImageUrl")]
     pub default_thumbnail_image_url: Option<String>,
-    #[serde(rename = "concurrentUserCount")]
     pub concurrent_user_count: u32,
-    #[serde(rename = "accumulateCount")]
     pub accumulate_count: u32,
-    #[serde(rename = "openDate")]
     pub open_date: String,
-    #[serde(rename = "liveId")]
     pub live_id: i32,
     pub adult: bool,
     pub tags: Vec<String>,
-    #[serde(rename = "chatChannelId")]
     pub chat_channel_id: String,
-    #[serde(rename = "categoryType")]
     pub category_type: String,
-    #[serde(rename = "liveCategory")]
     pub live_category: String,
-    #[serde(rename = "liveCategoryValue")]
     pub live_category_value: String,
-    // #[serde(rename = "dropsCampaignNo")]
     // pub drops_campaign_no: Unknown,
-    // #[serde(rename = "watchPartyNo")]
     // pub watch_party_no: Unknown,
-    // #[serde(rename = "watchPartyTag")]
     // pub watch_party_tag: Unknown,
-    #[serde(rename = "channelId")]
     pub channel_id: ChannelId,
-    #[serde(rename = "livePlayvackJson")]
     pub live_playvack_json: String,
-    // #[serde(rename = "blindType")]
     // pub blind_type: Unknown,
 }
