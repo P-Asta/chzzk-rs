ab023e2c8baedb744a9c49a2db5a1910bc2b9abc
Asta(아스타) <86705803+P-Asta@users.noreply.github.com>
2025-03-14 09:25:59 +0900

    refactor(data/user): remove unnecessary code

--- chzzk/src/data/user.rs
+++ chzzk/src/data/user.rs
@@ -1,21 +1,15 @@
 use super::channel::ChannelId;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash, Default)]
+#[serde(rename_all="camelCase")]
 pub struct User {
-    #[serde(rename = "hasProfile")]
     pub has_profile: bool,
-    #[serde(rename = "userIdHash")]
     pub user_id_hash: ChannelId,
     pub nickname: String,
-    #[serde(rename = "profileImageUrl")]
     pub profile_image_url: String,
     // pub penalties: Unknown,
-    #[serde(rename = "officialNotiAgree")]
     pub official_noti_agree: bool,
-    #[serde(rename = "officialNotiAgreeUpdatedDate")]
     pub official_noti_agree_updated_date: String,
-    #[serde(rename = "verifiedMark")]
     pub verified_mark: bool,
-    #[serde(rename = "loggedIn")]
     pub logged_in: bool,
 }
