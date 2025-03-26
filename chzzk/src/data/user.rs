9d6d6e76f7aa3cc173e900cb45c11a272d83f4e2
5-23 <yhanbyeol6@gmail.com>
2025-03-26 09:27:22 +0900

    fix(user): critical bug fix

--- chzzk/src/data/user.rs
+++ chzzk/src/data/user.rs
@@ -1,7 +1,9 @@
+use serde::{Deserialize, Deserializer};
+
 use super::channel::ChannelId;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash, Default)]
-#[serde(rename_all="camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct User {
     pub has_profile: bool,
     pub user_id_hash: ChannelId,
@@ -9,7 +11,17 @@ pub struct User {
     pub profile_image_url: String,
     // pub penalties: Unknown,
     pub official_noti_agree: bool,
+    #[serde(deserialize_with = "null_to_default")]
     pub official_noti_agree_updated_date: String,
     pub verified_mark: bool,
     pub logged_in: bool,
+}
+
+fn null_to_default<'de, D, T>(de: D) -> Result<T, D::Error>
+where
+    D: Deserializer<'de>,
+    T: Default + Deserialize<'de>,
+{
+    let key = Option::<T>::deserialize(de)?;
+    Ok(key.unwrap_or_default())
 }
